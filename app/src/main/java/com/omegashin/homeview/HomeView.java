9a9346fe7ac53d2cc98317efed0de3de1c18dae2
bdcorps <bdcorps@gmail.com>
2017-01-01 01:11:31 -0500

    Version 1.0

--- app/src/main/java/com/omegashin/homeview/HomeView.java
+++ app/src/main/java/com/omegashin/homeview/HomeView.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class HomeView extends AppCompatActivity {
 
@@ -25,5 +26,4 @@ public class HomeView extends AppCompatActivity {
             }
         });
     }
-
 }
