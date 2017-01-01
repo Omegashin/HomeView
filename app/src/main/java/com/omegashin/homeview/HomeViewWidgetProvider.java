833756b8032e5fde873420c705db1803f67f620f
omegashin <gdesigner.arshdeep@gmail.com>
2016-12-31 19:29:45 -0500

    Initial commit

---
+++ app/src/main/java/com/omegashin/homeview/HomeViewWidgetProvider.java
@@ -0,0 +1,10 @@
+package com.omegashin.homeview;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by gdesi on 31-Dec-16.
+ */
+
+public class HomeViewWidgetProvider extends AppWidgetProvider {
+}
